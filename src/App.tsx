0c00ea20195aec90d8fcae5e62c3f730286a1fac
flamarionfagundes <flamarion.fagundes@lighthouseot.com.br>
2021-12-13 11:43:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Hello World!</Text>
+    </View>
+  )
+}
+
+export default App
